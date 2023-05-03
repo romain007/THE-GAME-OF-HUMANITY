e9e8b6ee5c4ffcd2bdc25cd40f8ca9fca20b944f
ORAMI <55509423+romain007@users.noreply.github.com>
2023-05-03 11:59:15 +0200

    Update data_managment.py

--- Data/data_managment.py
+++ Data/data_managment.py
@@ -4,26 +4,8 @@ from math import *
 #Sert à récupérer des donnés dans un fichier ou à y écrire, tous ceux qui est data
 #Fait par ANTOINE
 
-def lire(ligne,fichier):
-    """ retourne le contenu de la ligne "i" du fichier spécifié, renvoie False si la ligne est vide """
-    
-    with open(fichier,'r') as file:
-        line_content = file.readlines()[ligne-1]
-        if line_content == '':
-            return False
-        return line_content
-        
-def ecrire(ligne,contenu,fichier):
-    """écrit à la ligne "i" dans le fichier spécifié avec le contenu entré"""
-    
-    with open(fichier,'r') as file:
-        file_content = file.readlines()
-        file_content[ligne-1] = str(contenu)
-        file_content = ''.join(file_content)
-    with open(fichier,'w') as file:
-        file.write(file_content)
-
 def diviseurs_communs(nombre1, nombre2):
+    """Retourne les diviseurs communs entre 2 nombres. En entré les 2 nombres et en sortie une liste de diviseur commun""")
     diviseurs1 = set()
     for i in range(1, nombre1 + 1):
         if nombre1 % i == 0:
@@ -39,7 +21,7 @@ def diviseurs_communs(nombre1, nombre2):
 
 
 def melange_dico(dictionnaire):
-
+    """Melange un dictionnaire en changeant de position les paires clé/valeurs. Prend en entré un dictionnaire et en sortie le dictionnaire mélangé"""
     # Création d'une liste contenant les paires clé-valeur du dictionnaire
     liste_paires = list(dictionnaire.items())
 
@@ -50,12 +32,14 @@ def melange_dico(dictionnaire):
     return dict(liste_paires)
 
 def signe(entry):
+    #Retourne le signe de l'entry
     if entry >= 0:
         return 1
     if entry < 0:
         return -1
     
 def distance_euclidienne(depart,arrive):
+    """Distance euclidienne entre 2 points depart et arrive en entrée"""
     return int(sqrt( (depart[0]-arrive[0])**2 + (depart[1]-arrive[1])**2   ))    
 
 
