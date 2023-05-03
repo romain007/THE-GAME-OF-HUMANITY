26ef142010e503cfcc845548e16c11a6be3bf92c
ORAMI <55509423+romain007@users.noreply.github.com>
2023-05-03 12:01:01 +0200

    Update gestion.py

--- Environment/gestion.py
+++ Environment/gestion.py
@@ -2,25 +2,27 @@ from Map.generation import *
 from Environment.case_occupe import *
 
 def kill(perso):
+    """Tue tous les perso définis en entrée"""
     for position in map:
         if map[position]["objet"] == perso:
             map[position] = {"objet":"grass","IDENTIFIANT":"grass123"}
 
 def reset_camp():
+    """Met à jour les camps"""
     #Liste des camps de tous les personnages
 
     liste_id = all_player(param=["player_red","player_blue"])
 
-    #Fais spawn les perso bb dans leur nouveaux campements
+    #Fais spawn les perso bb dans leurs nouveaux campements
     for perso in campement:
         if not campement[perso]["active"] and campement[perso]["parent"] in liste_id:
-            campement[perso]["active"] = True
+            campement[perso]["active"] = True  #On active le campement (permet de l'afficher)
             map[campement[perso]["position"] ]= campement[perso]["baby"]
             campement[perso]["baby"] = False
 
     liste_id = all_player(param=["player_red","player_blue"])
 
-    #Active  les camps des enfants dont les parents ne sont pas mort
+    #Supprime les camps attribué a des persos mort
     for perso in campement.copy():
         if perso not in liste_id:
             del campement[perso] #On supprime son campement
