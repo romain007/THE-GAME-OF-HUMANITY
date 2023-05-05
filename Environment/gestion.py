83779b7a02181fb24877ddec8a23b9d39cac8d16
ORAMI <55509423+romain007@users.noreply.github.com>
2023-05-06 00:02:02 +0200

    Update gestion.py

--- Environment/gestion.py
+++ Environment/gestion.py
@@ -27,18 +27,41 @@ def reset_camp():
         if perso not in liste_id:
             del campement[perso] #On supprime son campement
 
+def restart(val=100):
+    # Remet à 100 l'energie des persos qui sont rentrés
+    for pos in map:
+        if map[pos]["objet"] in ["player_red","player_blue"] and pos == map[pos]["CAMP"]:
+            map[pos]["ENERGY"] = val
+    #Les persos qui n'ont pas mangé au moins 1 poulet ne survivent pas. Cette option n'est pas fonctionnel car cela rend la simulation beaucoup moins bien
+    """for pos in map:
+        if map[pos]["objet"] in ["player_red","player_blue"]:
+            if map[pos]["FOOD"] == 0:
+                map[pos] = {"objet":"grass","IDENTIFIANT":"grass"+str(id(map[pos]))}
+            else:
+                map[pos]["FOOD"] = 0"""
+    
+def energy_compteur():
+    """Supprime les personnages qui ont moins de 0 d'énergie"""
+    mort = []
+            
+        #Tue ceux qui en ont moins de 0
+        if map[pos]["objet"] in ["player_red","player_blue"] and map[pos]["ENERGY"] < 0:
+            mort.append({pos:map[pos]["objet"]})
+            map[pos] = {"objet":"grass","IDENTIFIANT":"grass"+str(id(map[pos]))}
+    return mort
 
-#Donne une position vide au hasard sur la map
+
 def hasard_camp():
-    while 1 == 1:
+    """Donne une position vide au hasard sur la map"""
+    while True:
         hasard  = random.randint(0,largeur-1)
         hasard2 = random.randint(0,hauteur-1)
         if map[(hasard,hasard2)]["objet"] == "grass":
             #Camp sur un endroit aléatoire
             return (hasard,hasard2)
 
-#Permet de réinitialiser le nombre de poulet mangé pour tous les personnages
 def reset_food():
+    """Permet de réinitialiser le nombre de poulet mangé pour tous les personnages"""
     for i in map:
         if map[i] in ["player_blue","player_red"]:
             map[i]["FOOD"] = 0
