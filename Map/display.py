bb05139151f1ac0750a0d0d4620be0661483cf44
ORAMI <55509423+romain007@users.noreply.github.com>
2023-05-06 00:27:31 +0200

    Update display.py

--- Map/display.py
+++ Map/display.py
@@ -5,10 +5,11 @@ import pygame
 import sys
 
 #Fait par ROMAIN
-
+#Ce module sert uniquement à l'affichage, il n'apporte rien au jeu mais se contente de l'afficher avec pygame
 screen = pygame.display.set_mode((info_w, info_h))
 
 def en_fonction_var(objet):
+    """Permet de changer le sprite du personnage en fonction de sa variable la plus élevés. Son sprite n'est donc pas définis à l'avance mais au moment de l'affichage, il peut donc évolué au cours du temps."""
     if objet["objet"] == "food":
         return "food"
     liste = [objet['SPEED'],objet['POWER'],objet['AGILITY'],objet["FERTILITE"]]
@@ -25,8 +26,13 @@ def en_fonction_var(objet):
 
 
 def affichage(mort,temps=0,gamma=0.5,c=0):
-
-
+    """Fonction centrale qui est composé des paramètres :
+    -mort -> Affiche les personnages mort meme si il ne sont plus dans le dictionnaire "map". Permet de faire en sorte qu'il ne disparaisse pas d'un coup, mais qu'il y ait une animation quand il meurt
+    -temps -> Permet d'avoir une vitesse de jeu rapide ou lente
+    -gamma -> Pour baisser la luminosité la nuit
+    -c -> compteur du nombre de jour
+    Elle ne renvoie rien en sortie mais se contente d'afficher tous le dictionnaire "map" avec pygame"""
+    
     pygame.display.set_gamma(gamma)
     font = pygame.font.Font(None, int(parametre["ZOOM"]*2))
     font2 = pygame.font.Font(None, 40)
@@ -75,6 +81,7 @@ def affichage(mort,temps=0,gamma=0.5,c=0):
 
 
 def val_inter(old_position,new_pos):
+    """A partir d'une position initiale et finale (la case d'a cotés), renvoie une liste de toutes les positions intermédiaire entre ces 2 cases.Permet l'animation du jeu"""
     #Définition des vecteurs initiaux et finaux. Les transforme en point a taille réel
     old_vector = pygame.math.Vector2(old_position[0]*ecart,old_position[1]*ecart)
     new_vector = pygame.math.Vector2(new_pos[0]*ecart,new_pos[1]*ecart)
